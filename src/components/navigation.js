c0e67ea7abd9488234214ac8346f6cc1dbf8cff9
chagers <cehager@gmail.com>
2020-04-04 19:59:47 -0700

    Still working in uikit

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import eggplant from '../assets/eggplant.png';
 
 export default () => (
   <nav role="navigation" className="uk-navbar-container" uk-navbar="true">
     <div className="uk-navbar-left">
+      <div className="uk-navbar-item uk-logo">
+        <img src={eggplant} alt="eggplant" />
+      </div>
+      <div className="uk-navbar-item">Feed the Gillmores</div>
+    </div>
+    <div className="uk-navbar-right">
         <ul className="uk-navbar-nav">
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/">Homer</Link>
             </li>
             <li>
               <Link to="/recipe-list">Recipe List</Link>
